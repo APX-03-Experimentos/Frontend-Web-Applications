d765dd579b0cb0ad5b07cb99de852cb6b57836fc
sebastianrealcalderon <u20221d964@upc.edu.pe>
2025-10-07 10:57:25 -0500

    feat: created course members page

---
+++ src/app/courses/pages/course-members-page/course-members-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-members-page',
+  imports: [],
+  templateUrl: './course-members-page.html',
+  standalone: true,
+  styleUrl: './course-members-page.css'
+})
+export class CourseMembersPage {
+
+}
