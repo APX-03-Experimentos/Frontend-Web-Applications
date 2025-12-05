a148c53d704a8c0e301620bf9147c869d107fbe1
sebastianrealcalderon <u20221d964@upc.edu.pe>
2025-12-05 16:26:18 -0500

    fix: api/v1

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  serverBaseUrl: "https://backend-web-services-microservices-xtnu.onrender.com",
+  serverBaseUrl: "https://backend-web-services-microservices-xtnu.onrender.com/api/v1",
   authenticationEndpointPath: '/authentication',
   usersEndpointPath: '/users',
   coursesEndpointPath: '/courses',
