2da20e3a85e3443d9cf27ac0b19a0cbc95be9dad
Firtness <u202122484@upc.edu.pe>
2025-12-04 21:40:21 -0500

    fix(joinCourse): fix joinCourse

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -30,7 +30,7 @@ export class CoursesService extends BaseService<Course> {
   }
 
   JoinCourse(key: string): Observable<Course> {
-    return this.http.post<Course>(`${this.resourcePath()}/join/${key}`, {}, this.httpOptions)
+    return this.http.get<Course>(`${this.resourcePath()}/join/${key}`, this.httpOptions)
   }
 
   KickStudentFromCourse(courseId: number, studentId: number): Observable<void> {
